38ef4a5948b846179c00f36f1c6961bae40bcc51
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-06-13 22:42:48 +1000

    Revert "changed rate conversion algorithm to the one used in Wohlstand's NukedOPN2 extension"
    This reverts commit 6af4a6bacfb8e797cbc597b82723018221524d70.

--- vgmcore/src/vgm_chip_rateconv.cpp
+++ vgmcore/src/vgm_chip_rateconv.cpp
@@ -2,44 +2,85 @@
 #include <string.h>
 #include <cmath>
 
-vgm_chip_rateconv::vgm_chip_rateconv(size_t num_channels, float f_in, bool stereo_channels) : _stereo(stereo_channels), _rate_ratio(44100.0 / f_in), vgm_chip(num_channels, stereo_channels) {
-    _samples_left = new float[num_channels];
-    _old_samples_left = new float[num_channels];
-    if(stereo_channels) {
-        _samples_right = new float[num_channels];
-        _old_samples_right = new float[num_channels];
+#define _PI_ 3.14159265358979323846264338327950288
+
+// #define SINCTAB_AMP                             (1.0 / 0.091879) // amplitude to amplify the sinc table up to (so that input data of range -1.0 .. 1.0 gets transformed to -1.0 .. 1.0)
+
+static double _blackman(double x) {
+    return 0.42 - 0.5 * cos(2 * _PI_ * x) + 0.08 * cos(4 * _PI_ * x);
+}
+
+static double _sinc(double x) {
+    return (x == 0.0) ? 1.0 : sin(_PI_ * x) / (_PI_ * x);
+}
+
+static double _windowed_sinc(double x) {
+    return _blackman(0.5 + 0.5 * x / (RATECONV_LW / 2)) * _sinc(x);
+}
+
+void vgm_chip_rateconv::reset_rateconv() {
+    memset(_buf, 0, sizeof(float) * channels * RATECONV_LW * ((_stereo) ? 2 : 1));
+}
+
+vgm_chip_rateconv::vgm_chip_rateconv(size_t num_channels, float f_in, bool stereo_channels) : _stereo(stereo_channels), _f_ratio(f_in / 44100.0), vgm_chip(num_channels, stereo_channels) {
+    _buf = new float[num_channels * RATECONV_LW * ((stereo_channels) ? 2 : 1)];
+    reset_rateconv();
+
+    /* create sinc table */
+    for(int i = 0; i < RATECONV_SINC_RESO * RATECONV_LW / 2; i++) {
+        const double x = (double)i / RATECONV_SINC_RESO;
+        if(44100.0 < f_in) {
+            /* downsampling */
+            _sinctab[i] = _windowed_sinc(x / _f_ratio) / _f_ratio;
+        } else {
+            _sinctab[i] = _windowed_sinc(x);
+        }
+        // _sinctab[i] *= SINCTAB_AMP;
     }
 }
 
 vgm_chip_rateconv::~vgm_chip_rateconv() {
-    delete _samples_left; delete _old_samples_left;
-    if(_stereo) {
-        delete _samples_right; delete _old_samples_right;
-    }
+    delete _buf;
+}
+
+static inline float lookup_sinc_table(const float* table, double x) {
+    int idx = (int)(x * RATECONV_SINC_RESO);
+    if(idx < 0) idx = -idx;
+    if(idx > RATECONV_SINC_RESO * RATECONV_LW / 2 - 1) idx = RATECONV_SINC_RESO * RATECONV_LW / 2 - 1;
+    return table[idx];
 }
 
 void vgm_chip_rateconv::put_sample(size_t channel, float val, bool side) {
     if(channel >= channels) return;
-    if(!_stereo) side = false;
-    if(side) {
-        _old_samples_right[channel] = _samples_right[channel];
-        _samples_right[channel] = val;
-    } else {
-        _old_samples_left[channel] = _samples_left[channel];
-        _samples_left[channel] = val;
-    }
+    float* buf = (_stereo) ? &_buf[channel * 2 * RATECONV_LW + ((side) ? 1 : 0)] : &_buf[channel * RATECONV_LW];
+    memmove(buf, &buf[1], sizeof(float) * (RATECONV_LW - 1));
+    buf[RATECONV_LW - 1] = val;
 }
 
 void vgm_chip_rateconv::next_sample() {
-    while(_sample_cnt >= _rate_ratio) {
-        clock();
-        _sample_cnt -= _rate_ratio;
-    }
+    _timer += _f_ratio;
+    int num_clocks = floor(_timer);
+    double dn = _timer - num_clocks;
+    _timer = dn;
+
+    for(int i = 0; i < num_clocks; i++) clock();
 
     for(int i = 0; i < channels; i++) {
-        _channels_out_left[i] = (_old_samples_left[i] * (_rate_ratio - _sample_cnt) + _samples_left[i] * _sample_cnt) / _rate_ratio;
-        if(_stereo) _channels_out_right[i] = (_old_samples_right[i] * (_rate_ratio - _sample_cnt) + _samples_right[i] * _sample_cnt) / _rate_ratio;
+        if(_stereo) {
+            float* buf = &_buf[i * 2 * RATECONV_LW];
+            _channels_out_left[i] = _channels_out_right[i] = 0;
+            for(int k = 0; k < RATECONV_LW; k++) {
+                double x = ((double)k - (RATECONV_LW / 2 - 1)) - dn;
+                _channels_out_left[i] += buf[k << 1] * lookup_sinc_table(_sinctab, x);
+                _channels_out_right[i] += buf[(k << 1) | 1] * lookup_sinc_table(_sinctab, x);
+            }
+        } else {
+            float* buf = &_buf[i * RATECONV_LW];
+            _channels_out[i] = 0;
+            for(int k = 0; k < RATECONV_LW; k++) {
+                double x = ((double)k - (RATECONV_LW / 2 - 1)) - dn;
+                _channels_out[i] += buf[k] * lookup_sinc_table(_sinctab, x);
+            }
+        }
     }
-
-    _sample_cnt++;
 }
